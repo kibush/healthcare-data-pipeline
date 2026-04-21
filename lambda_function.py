d50100cacf754203a24bd48dcca22b84c338eee4
Kenneth Kibuga <Kibush2000@yahoo.com>
2026-04-20 21:16:30 -0400

    Add DynamoDB storage and validation pipeline

--- lambda_function.py
+++ lambda_function.py
@@ -5,6 +5,23 @@ from datetime import datetime
 
 s3 = boto3.client('s3')
 
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('PatientRecords')
+
+#simpler parser(conver text ->dictionary )
+
+def parse_patient_data(content):
+    data = {}
+
+    for line in content.split("\n"):
+        if ":" in line:
+            key, value = line.split(":", 1)
+            data[key.strip()] = value.strip()
+
+    return data
+
+
+
 def lambda_handler(event, context):
     print("Event:", json.dumps(event))
 
@@ -20,3 +37,16 @@ def lambda_handler(event, context):
     content = response['Body'].read().decode('utf-8')
 
     print("File content:", content)
+
+    parsed_data = parse_patient_data(content)
+    print("Parsed data:", parsed_data)
+
+    # ✅ Ensure required key exists
+    if 'PatientID' not in parsed_data:
+        print("Missing PatientID — skipping record")
+        return
+
+    #DynamoDB saver 
+    parsed_data['ProcessedAt'] = datetime.utcnow().isoformat()
+    table.put_item(Item=parsed_data)
+    print("Data saved to DynamoDB")
